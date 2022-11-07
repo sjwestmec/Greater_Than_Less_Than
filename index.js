61c1a6bb915d6ab5cf712795f6ce1827f2e4de12
sjwestmec <sbarba205@west-mec.org>
2022-11-07 16:49:59 -0700

    started the js portion (non functional)

---
+++ index.js
@@ -0,0 +1,11 @@
+//getRandomInt("num")
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+console.log(getRandomInt(100));
+
+let greaterThan
+console.log(x > y)
+
+let lessThan
+console.log(x < y)
